75893124f84b640d0e119d7ad466ca02df44e255
hngfu <djsl1018@naver.com>
2018-09-17 15:03:12 +0900

    [사다리게임] 초기화함수, Int변환함수 추가
    참가인원 수와 사다디의 최대 높이를 입력받는 함수, Stirng값을 입력해주면 Int값으로 바꿔주는 함수를 구현하였습니다.

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,3 +8,20 @@
 
 import Foundation
 
+func initialize() -> String {
+    print("참여할 사람은 몇 명 인가요?")
+    guard let participant = readLine() else { return "입력값을 확인해 주세요." }
+    print("최대 사다리 높이는 몇 개인가요?")
+    guard let maximumLadderHeight = readLine() else { return "입력값을 확인해 주세요." }
+    
+    print(participant,maximumLadderHeight)
+    return ""
+}
+
+//입력: "12" -> 출력: 12(Int), 입력: "asd" -> 출력: 0
+func convertToInt(_ valueToConvert:String) -> Int {
+    guard let convertedValue = Int(valueToConvert) else { return 0 }
+    return convertedValue
+}
+
+print(convertToInt("dfg"))
